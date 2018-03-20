e954705d524d1492bf8d7e26522f373a37686f83
dmy@cnksi.com <dmy@cnksi.com>
2018-03-20 16:14:53 +0800

    初始化项目

---
+++ src/page/aView.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class aView extends React.Component {
+  render () {
+    return (
+      <div>a页面</div>
+    )
+  }
+}
---
+++ src/page/bView.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class bView extends React.Component {
+  render () {
+    return (
+      <div>b页面</div>
+    )
+  }
+}
